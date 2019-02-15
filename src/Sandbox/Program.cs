f33262cfb7736cc06ae2087849248dfc878c0719
Petar <p.panayotov94@gmail.com>
2019-02-15 16:46:20 +0200

    Created Sandbox app for testing

---
+++ src/Sandbox/Program.cs
@@ -0,0 +1,49 @@
+﻿using FunApp.Web.Data;
+using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+namespace Sandbox
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{typeof(Program).Namespace} ({string.Join(" ", args)}) starts working...");
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider(true);
+
+            using (var serviceScope = serviceProvider.CreateScope())
+            {
+                serviceProvider = serviceScope.ServiceProvider;
+                SandboxCode(serviceProvider);
+            }
+        }
+
+        private static void SandboxCode(IServiceProvider serviceProvider)
+        {
+            var db = serviceProvider.GetService<ApplicationDbContext>();
+            Console.WriteLine(db.Users.Count());
+        }
+
+        private static void ConfigureServices(ServiceCollection services)
+        {
+            var configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection")));
+        }
+    }
+}
